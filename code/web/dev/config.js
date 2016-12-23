3a8f2a704cafac1bb414893d655b929621d09d27
Anton Glebov <a.glebov.box@gmail.com>
2016-12-23 18:01:31 +0200

    Added beta version of 'Config' page.

---
+++ code/web/dev/config.js
@@ -0,0 +1,64 @@
+var output;
+
+function init() {
+	// Основной метод
+	output = document.getElementById("messages");
+	startWebSocket();
+}
+
+function startWebSocket() {
+	// Инициализация socket-соединения
+	var wsUri = "ws://" + location.host + "/";
+	websocket = new WebSocket(wsUri);
+	websocket.onopen	= function(evt) {onOpen(evt)};
+	websocket.onclose   = function(evt) {onClose(evt)};
+	websocket.onmessage = function(evt) {onMessage(evt)};
+	websocket.onerror   = function(evt) {onError(evt)};
+}
+
+function onOpen(evt) {
+	// Действие при установлении socket-соединения
+	websocket.send(0x00);
+}
+function onMessage(evt) {
+	// Действие при получении информации в текстовом виде
+	processJSON(evt.data);
+}
+function onError(evt) {
+	// Действие при возникновении ошибки
+	writeToScreen('<div class="alert alert-danger" role="alert">Connection error!</div>');
+}
+function onClose(evt) {
+	// Действие при разъединении socket-соединения
+}
+
+function doSend(message) {
+	// Метод отправки информации в текстовом виде 
+	websocket.send(message);
+}
+function doDisconnect() {
+	// Метод разрыва socket-соединения
+	websocket.close();
+}
+
+function processJSON(msg) {
+	// Метод обработки информации, полученной из JSON
+	var data = JSON.parse(msg);
+	$.each(data, function(key, val) {
+		$.each(val, function(subkey, subval) {
+			var target = $("#" + subkey);
+			switch (subkey) {				
+				default:
+					document.getElementById(subkey).innerHTML = subval;
+					break;
+			}
+		});
+	});
+}
+
+function writeToScreen(message) {
+	// Метод вывода информации на страницу
+	output.innerHTML += message;	
+}
+
+window.addEventListener("load", init, false);
